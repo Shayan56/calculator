53e27c75be4d29a6bb332f158faf5a387f281150
Shayan56 <122144579+Shayan56@users.noreply.github.com>
2025-08-20 10:00:43 +0530

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,23 @@
+# test_calculator.py
+import unittest
+from shayan import Calculator   # import your Calculator class
+
+class CalculatorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(10, self.calc.add(3, 7))
+
+    def test_subtract(self):
+        self.assertEqual(12, self.calc.subtract(15, 3))
+
+    def test_multiply(self):
+        self.assertEqual(30, self.calc.multiply(5, 6))
+
+    def test_divide(self):
+        self.assertEqual(3, self.calc.divide(6, 2))
+
+if __name__ == '__main__':
+    unittest.main()
